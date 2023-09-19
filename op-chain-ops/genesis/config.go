f29cc1dabdd5c0f1fba3bd2448e5e72f624c1fc2
Karl Bartel <karl.bartel@clabs.co>
2023-09-19 14:39:45 +0200

    Enable cel2 fork by default

--- op-chain-ops/genesis/config.go
+++ op-chain-ops/genesis/config.go
@@ -1024,6 +1024,7 @@ func (d *DeployConfig) RollupConfig(l1StartBlock *types.Header, l2GenesisBlockHa
 		InteropTime:             d.InteropTime(l1StartTime),
 		ProtocolVersionsAddress: d.ProtocolVersionsProxy,
 		AltDAConfig:             altDA,
+		Cel2Time:                d.RegolithTime(l1StartTime),
 	}, nil
 }
 
