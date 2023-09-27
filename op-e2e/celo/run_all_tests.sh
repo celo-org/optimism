40da1bbe3560632ab7ee01bec475bd8f13321cc5
Karl Bartel <karl.bartel@clabs.co>
2023-09-27 14:44:56 +0200

    Add token duality e2e test
    based on https://github.com/celo-org/op-geth/pull/21

---
+++ op-e2e/celo/run_all_tests.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eo pipefail
+
+SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
+source "$SCRIPT_DIR/shared.sh"
+
+## Start geth
+cd "$SCRIPT_DIR/../.." || exit 1
+trap 'cd "$SCRIPT_DIR/../.." && make devnet-down' EXIT  # kill bg job at exit
+make devnet-up
+
+# Wait for geth to be ready
+for _ in {1..10}
+do
+	if cast block &> /dev/null
+	then
+		break
+	fi
+	sleep 0.2
+done
+
+## Run tests
+echo Geth ready, start tests
+failures=0
+tests=0
+cd "$SCRIPT_DIR" || exit 1
+for f in test_*
+do
+	echo -e "\nRun $f"
+	if "./$f"
+	then
+		tput setaf 2 || true
+		echo "PASS $f"
+	else
+		tput setaf 1 || true
+		echo "FAIL $f ❌"
+		((failures++)) || true
+	fi
+	tput init || true
+	((tests++)) || true
+done
+
+## Final summary
+echo
+if [[ $failures -eq 0 ]]
+then
+	tput setaf 2 || true
+	echo All tests succeeded!
+else
+	tput setaf 1 || true
+	echo $failures/$tests failed.
+fi
+tput init || true
+exit $failures
