14a9446dd3e9bf534ea94e1e489c49b5e503fb8a
Karl Bartel <karl.bartel@clabs.co>
2023-11-11 18:54:10 +0100

    Adapt test to recent cast version
    cast go a bit stricter with regards to function signatures.

--- op-e2e/celo/test_token_duality.sh
+++ op-e2e/celo/test_token_duality.sh
@@ -6,7 +6,7 @@ source shared.sh
 
 # Send token and check balance
 balance_before=$(cast balance 0x000000000000000000000000000000000000dEaD)
-cast send --private-key $ACC_PRIVKEY $TOKEN_ADDR 'function transfer(address to, uint256 value) external returns (bool)' 0x000000000000000000000000000000000000dEaD 100
+cast send --private-key $ACC_PRIVKEY $TOKEN_ADDR 'function transfer(address to, uint256 value) returns (bool)' 0x000000000000000000000000000000000000dEaD 100
 balance_after=$(cast balance 0x000000000000000000000000000000000000dEaD)
 echo "Balance change: $balance_before -> $balance_after"
 [[ $((balance_before + 100)) -eq $balance_after ]] || (echo "Balance did not change as expected"; exit 1)
