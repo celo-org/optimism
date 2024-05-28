b9affb34f363ccf4a91041fe778e0473aad34051
Karl Bartel <karl.bartel@clabs.co>
2024-05-28 17:11:16 +0200

    Disable SortedOracles in Genesis

--- packages/contracts-bedrock/scripts/L2Genesis.s.sol
+++ packages/contracts-bedrock/scripts/L2Genesis.s.sol
@@ -677,7 +677,7 @@ contract L2Genesis is Deployer {
         setCeloFeeHandler();
         setCeloMentoFeeHandlerSeller();
         setCeloUniswapFeeHandlerSeller();
-        setCeloSortedOracles();
+        // setCeloSortedOracles();
         // setCeloAddressSortedLinkedListWithMedian();
         setCeloFeeCurrency();
     }
