61818197152b00b028c2c04d0ff787e256bda9ab
alecps <alec@cLabs.co>
2024-06-14 14:07:13 -0400

    add ability to run block and state migration seperately or together

--- op-chain-ops/cmd/celo-migrate/main.go
+++ op-chain-ops/cmd/celo-migrate/main.go
@@ -48,12 +48,12 @@ var (
 		Usage:    "Path to write the rollup config JSON file, to be provided to op-node with the 'rollup.config' flag",
 		Required: true,
 	}
-	oldDBPathFlag = &cli.StringFlag{
+	oldDBPathFlag = &cli.PathFlag{
 		Name:     "old-db",
 		Usage:    "Path to the old Celo chaindata",
 		Required: true,
 	}
-	newDBPathFlag = &cli.StringFlag{
+	newDBPathFlag = &cli.PathFlag{
 		Name:     "new-db",
 		Usage:    "Path to write migrated Celo chaindata",
 		Required: true,
@@ -68,7 +68,7 @@ var (
 		Usage: "Memory limit in MB",
 		Value: 7500,
 	}
-	dryRunFlag = &cli.BoolFlag{ // TODO
+	dryRunFlag = &cli.BoolFlag{ // TODO(Alec)
 		Name:  "dry-run",
 		Usage: "Dry run the upgrade by not committing the database",
 	}
@@ -81,20 +81,24 @@ var (
 		Usage: "Use to keep migrated ancients, but not non-ancients",
 	}
 
-	flags = []cli.Flag{
+	blockMigrationFlags = []cli.Flag{
+		oldDBPathFlag,
+		newDBPathFlag,
+		batchSizeFlag,
+		memoryLimitFlag,
+		clearAllFlag,
+		clearNonAncientsFlag,
+	}
+	stateMigrationFlags = []cli.Flag{
+		newDBPathFlag,
 		deployConfigFlag,
 		l1DeploymentsFlag,
 		l1RPCFlag,
 		l2AllocsFlag,
 		outfileRollupConfigFlag,
-		oldDBPathFlag,
-		newDBPathFlag,
-		batchSizeFlag,
-		memoryLimitFlag,
 		dryRunFlag,
-		clearAllFlag,
-		clearNonAncientsFlag,
 	}
+	fullMigrationFlags = append(blockMigrationFlags, stateMigrationFlags[1:]...) // skip duplicate newDBPathFlag
 
 	// TODO: read those form the deploy config
 	// TODO(pl): select values
@@ -105,45 +109,100 @@ var (
 	OutFilePerm = os.FileMode(0o440)
 )
 
+type blockMigrationOptions struct {
+	oldDBPath        string
+	newDBPath        string
+	batchSize        uint64
+	memoryLimit      int64
+	clearAll         bool
+	clearNonAncients bool
+}
+
+type stateMigrationOptions struct {
+	deployConfig        string
+	l1Deployments       string
+	l1RPC               string
+	l2AllocsPath        string
+	outfileRollupConfig string
+	newDBPath           string
+	dryRun              bool
+}
+
+func parseBlockMigrationOptions(ctx *cli.Context) blockMigrationOptions {
+	return blockMigrationOptions{
+		oldDBPath:        ctx.String("old-db"),
+		newDBPath:        ctx.String("new-db"),
+		batchSize:        ctx.Uint64("batch-size"),
+		memoryLimit:      ctx.Int64("memory-limit"),
+		clearAll:         ctx.Bool("clear-all"),
+		clearNonAncients: ctx.Bool("clear-nonAncients"),
+	}
+}
+
+func parseStateMigrationOptions(ctx *cli.Context) stateMigrationOptions {
+	return stateMigrationOptions{
+		newDBPath:           ctx.String("new-db"),
+		deployConfig:        ctx.Path("deploy-config"),
+		l1Deployments:       ctx.Path("l1-deployments"),
+		l1RPC:               ctx.String("l1-rpc"),
+		l2AllocsPath:        ctx.Path("l2-allocs"),
+		outfileRollupConfig: ctx.Path("outfile.rollup-config"),
+		dryRun:              ctx.Bool("dry-run"),
+	}
+}
+
 func main() {
 
-	// Check that `rsync` command is available. We use this to copy the db excluding ancients, which we will copy separately
-	if _, err := exec.LookPath("rsync"); err != nil {
-		log.Error("Please install `rsync` to use this script")
-		return
-	}
+	color := isatty.IsTerminal(os.Stderr.Fd())
+	handler := log.NewTerminalHandlerWithLevel(os.Stderr, slog.LevelDebug, color)
+	oplog.SetGlobalLogHandler(handler)
 
 	app := &cli.App{
-		Name:  "migrate",
-		Usage: "Migrate Celo block data and state to a CeL2 DB",
-		Flags: flags,
-		Action: func(ctx *cli.Context) error {
-			deployConfig := ctx.Path("deploy-config")
-			l1Deployments := ctx.Path("l1-deployments")
-			l1RPC := ctx.String("l1-rpc")
-			l2AllocsPath := ctx.Path("l2-allocs")
-			outfileRollupConfig := ctx.Path("outfile.rollup-config")
-			// oldDBPath := ctx.String("old-db")
-			newDBPath := ctx.String("new-db")
-			// batchSize := ctx.Uint64("batch-size")
-			// memoryLimit := ctx.Int64("memory-limit")
-			dryRun := ctx.Bool("dry-run")
-			// clearAll := ctx.Bool("clear-all")
-			// clearNonAncients := ctx.Bool("clear-nonAncients")
+		Name:  "celo-migrate",
+		Usage: "Migrate Celo block and state data to a CeL2 DB",
+		Commands: []*cli.Command{
+			{
+				Name:    "blocks",
+				Aliases: []string{"b"},
+				Usage:   "Migrate Celo block data to a CeL2 DB",
+				Flags:   blockMigrationFlags,
+				Action: func(ctx *cli.Context) error {
+					return runBlockMigration(parseBlockMigrationOptions(ctx))
+				},
+			},
+			{
+				Name:    "state",
+				Aliases: []string{"s"},
+				Usage:   "Migrate Celo state data to a CeL2 DB. Makes necessary state changes and generates a rollup config file.",
+				Flags:   stateMigrationFlags,
+				Action: func(ctx *cli.Context) error {
+					return runStateMigration(parseStateMigrationOptions(ctx))
+				},
+			},
+			{
+				Name:    "all",
+				Aliases: []string{"a"},
+				Usage:   "Perform a full migration of both block and state data to a CeL2 DB",
+				Flags:   fullMigrationFlags,
+				Action: func(ctx *cli.Context) error {
+					if err := runBlockMigration(parseBlockMigrationOptions(ctx)); err != nil {
+						return fmt.Errorf("failed to run block migration: %w", err)
+					}
 
-			color := isatty.IsTerminal(os.Stderr.Fd())
-			handler := log.NewTerminalHandlerWithLevel(os.Stderr, slog.LevelDebug, color)
-			oplog.SetGlobalLogHandler(handler)
+					if err := runStateMigration(parseStateMigrationOptions(ctx)); err != nil {
+						return fmt.Errorf("failed to run state migration: %w", err)
+					}
 
-			// if err := runBlockMigration(oldDBPath, newDBPath, batchSize, memoryLimit, clearAll, clearNonAncients); err != nil {
-			// 	return fmt.Errorf("failed to run block migration: %w", err)
-			// }
-
-			if err := runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPath, outfileRollupConfig, dryRun); err != nil {
-				return fmt.Errorf("failed to run state migration: %w", err)
+					return nil
+				},
+			},
+		},
+		OnUsageError: func(ctx *cli.Context, err error, isSubcommand bool) error {
+			if isSubcommand {
+				return err
 			}
-
-			return nil
+			cli.ShowAppHelp(ctx)
+			return fmt.Errorf("Please provide a valid command")
 		},
 	}
 
@@ -153,33 +212,39 @@ func main() {
 	log.Info("Finished migration successfully!")
 }
 
-func runBlockMigration(oldDBPath, newDBPath string, batchSize uint64, memoryLimit int64, clearAll, clearNonAncients bool) error {
-	debug.SetMemoryLimit(memoryLimit * 1 << 20) // Set memory limit, converting from MB to bytes
+func runBlockMigration(opts blockMigrationOptions) error {
+
+	// Check that `rsync` command is available. We use this to copy the db excluding ancients, which we will copy separately
+	if _, err := exec.LookPath("rsync"); err != nil {
+		return fmt.Errorf("Please install `rsync` to run block migration")
+	}
+
+	debug.SetMemoryLimit(opts.memoryLimit * 1 << 20) // Set memory limit, converting from MB to bytes
 
 	var err error
 
-	if clearAll {
-		if err = os.RemoveAll(newDBPath); err != nil {
+	if opts.clearAll {
+		if err = os.RemoveAll(opts.newDBPath); err != nil {
 			return fmt.Errorf("failed to remove new database: %w", err)
 		}
 	}
-	if clearNonAncients {
-		if err = cleanupNonAncientDb(newDBPath); err != nil {
+	if opts.clearNonAncients {
+		if err = cleanupNonAncientDb(opts.newDBPath); err != nil {
 			return fmt.Errorf("failed to cleanup non-ancient database: %w", err)
 		}
 	}
 
-	if err = createEmptyNewDb(newDBPath); err != nil {
+	if err = createEmptyNewDb(opts.newDBPath); err != nil {
 		return fmt.Errorf("failed to create new database: %w", err)
 	}
 
 	var numAncientsNew uint64
-	if numAncientsNew, err = migrateAncientsDb(oldDBPath, newDBPath, batchSize); err != nil {
+	if numAncientsNew, err = migrateAncientsDb(opts.oldDBPath, opts.newDBPath, opts.batchSize); err != nil {
 		return fmt.Errorf("failed to migrate ancients database: %w", err)
 	}
 
 	var numNonAncients uint64
-	if numNonAncients, err = migrateNonAncientsDb(oldDBPath, newDBPath, numAncientsNew-1, batchSize); err != nil {
+	if numNonAncients, err = migrateNonAncientsDb(opts.oldDBPath, opts.newDBPath, numAncientsNew-1, opts.batchSize); err != nil {
 		return fmt.Errorf("failed to migrate non-ancients database: %w", err)
 	}
 
@@ -188,10 +253,10 @@ func runBlockMigration(oldDBPath, newDBPath string, batchSize uint64, memoryLimi
 	return nil
 }
 
-func runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPath, outfileRollupConfig string, dryRun bool) error {
+func runStateMigration(opts stateMigrationOptions) error {
 	// Read deployment configuration
-	log.Info("Deploy config", "path", deployConfig)
-	config, err := genesis.NewDeployConfig(deployConfig)
+	log.Info("Deploy config", "path", opts.deployConfig)
+	config, err := genesis.NewDeployConfig(opts.deployConfig)
 	if err != nil {
 		return err
 	}
@@ -204,17 +269,17 @@ func runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPa
 	}
 
 	// Try reading the L1 deployment information
-	deployments, err := genesis.NewL1Deployments(l1Deployments)
+	deployments, err := genesis.NewL1Deployments(opts.l1Deployments)
 	if err != nil {
-		return fmt.Errorf("cannot read L1 deployments at %s: %w", l1Deployments, err)
+		return fmt.Errorf("cannot read L1 deployments at %s: %w", opts.l1Deployments, err)
 	}
 	config.SetDeployments(deployments)
 
 	// Get latest block information from L1
 	var l1StartBlock *types.Block
-	client, err := ethclient.Dial(l1RPC)
+	client, err := ethclient.Dial(opts.l1RPC)
 	if err != nil {
-		return fmt.Errorf("cannot dial %s: %w", l1RPC, err)
+		return fmt.Errorf("cannot dial %s: %w", opts.l1RPC, err)
 	}
 
 	if config.L1StartingBlockTag == nil {
@@ -250,7 +315,7 @@ func runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPa
 	log.Info("Using L1 Start Block", "number", l1StartBlock.Number(), "hash", l1StartBlock.Hash().Hex())
 
 	// Build the L2 genesis block
-	l2Allocs, err := genesis.LoadForgeAllocs(l2AllocsPath)
+	l2Allocs, err := genesis.LoadForgeAllocs(opts.l2AllocsPath)
 	if err != nil {
 		return err
 	}
@@ -261,7 +326,7 @@ func runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPa
 	}
 
 	// Write changes to state to actual state database
-	cel2Header, err := applyStateMigrationChanges(l2Genesis, newDBPath, !dryRun)
+	cel2Header, err := applyStateMigrationChanges(l2Genesis, opts.newDBPath, !opts.dryRun)
 	if err != nil {
 		return err
 	}
@@ -275,8 +340,8 @@ func runStateMigration(newDBPath, deployConfig, l1Deployments, l1RPC, l2AllocsPa
 		return fmt.Errorf("generated rollup config does not pass validation: %w", err)
 	}
 
-	log.Info("Writing rollup config", "file", outfileRollupConfig)
-	if err := jsonutil.WriteJSON(outfileRollupConfig, rollupConfig, OutFilePerm); err != nil {
+	log.Info("Writing rollup config", "file", opts.outfileRollupConfig)
+	if err := jsonutil.WriteJSON(opts.outfileRollupConfig, rollupConfig, OutFilePerm); err != nil {
 		return err
 	}
 
