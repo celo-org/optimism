037473c128f4952f1b557bd7fc2f5043d0dd0ab5
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2024-06-24 11:31:13 +0200

    Fix deployCeloContracts config not set for celo devnet

--- bedrock-devnet/devnet/__init__.py
+++ bedrock-devnet/devnet/__init__.py
@@ -138,8 +138,9 @@ def init_devnet_l1_deploy_config(paths, update_timestamp=False):
         deploy_config['usePlasma'] = True
     if DEVNET_CELO:
         deploy_config['useCustomGasToken'] = True
-    #  Usage of the zero address in combination of the useCustomGasToken == True
-    # will deploy a new contract
+        deploy_config['deployCeloContracts'] = True
+        #  Usage of the zero address in combination of the useCustomGasToken == True
+        # will deploy a new contract
         deploy_config['customGasTokenAddress'] = "0x0000000000000000000000000000000000000000"
     write_json(paths.devnet_config_path, deploy_config)
 
