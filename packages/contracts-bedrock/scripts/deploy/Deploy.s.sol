250f4762a2788b556d03094af62d9c4c9f52be6d
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2024-06-26 12:53:51 +0200

    contracts: Add CeloTokenL1 chain assertions

--- packages/contracts-bedrock/scripts/deploy/Deploy.s.sol
+++ packages/contracts-bedrock/scripts/deploy/Deploy.s.sol
@@ -1325,15 +1325,6 @@ contract Deploy is Deployer {
 
         save("CustomGasToken", address(customGasToken));
         console.log("CustomGasToken deployed at %s", address(customGasToken));
-
-        // Override the `CustomGasToken` contract to the deployed implementation. This is necessary
-        // to check the `CustomGasToken` implementation alongside dependent contracts, which
-        // are always proxies.
-        Types.ContractSet memory contracts = _proxies();
-        contracts.CustomGasToken = address(customGasToken);
-        //TODO:
-        // ChainAssertions.checkOptimismPortal({ _contracts: contracts, _cfg: cfg, _isProxy: false });
-
         addr_ = address(customGasToken);
     }
 
@@ -1351,12 +1342,6 @@ contract Deploy is Deployer {
             _data: abi.encodeCall(CeloTokenL1.initialize, (portalProxyAddress))
         });
 
-        CeloTokenL1 customGasToken = CeloTokenL1(customGasTokenProxyAddress);
-        //TODO:
-        // string memory version = customGasTokenAddress.version();
-        // console.log("CustomGasToken version: %s", version);
-
-        //TODO:
-        // ChainAssertions.checkOptimismMintableERC20Factory({ _contracts: _proxies(), _isProxy: true });
+        ChainAssertions.checkCeloTokenL1({ _contracts: _proxies(), _isProxy: false });
     }
 }
