d359bc40cd69a1cbacda7377f118791c3d0fdcae
piersy <pierspowlesland@gmail.com>
2024-07-01 10:39:04 +0100

    Migration script fixes (#179)
    * Fixed migration for datadirs without ancients
    
    The script was assuming that ancients would have been migrated and was
    considering the numAncients-1 to be the next block to migrate but when
    numAncients is zero that's a problem.
    
    Also remved logic for  picking up where db migration left of for the
    level db since it was complicating the logic and that process takes a
    few seconds, which is nothing compared with the minutes taken to migrate
    the ancients.
    
    * Ensure that we set gas limit if migrating at pre-gingerbread point

--- op-chain-ops/cmd/celo-migrate/db.go
+++ op-chain-ops/cmd/celo-migrate/db.go
@@ -14,9 +14,8 @@ import (
 
 // Constants for the database
 const (
-	DBCache                        = 1024 // size of the cache in MB
-	DBHandles                      = 60   // number of handles
-	LastMigratedNonAncientBlockKey = "celoLastMigratedNonAncientBlock"
+	DBCache   = 1024 // size of the cache in MB
+	DBHandles = 60   // number of handles
 )
 
 var (
@@ -33,28 +32,6 @@ func encodeBlockNumber(number uint64) []byte {
 // headerKey = headerPrefix + num (uint64 big endian) + hash
 func headerKey(number uint64, hash common.Hash) []byte {
 	return append(append(headerPrefix, encodeBlockNumber(number)...), hash.Bytes()...)
-}
-
-// readLastMigratedNonAncientBlock returns the last migration number. If it doesn't exist, it returns 0.
-func readLastMigratedNonAncientBlock(db ethdb.KeyValueReader) uint64 {
-	data, err := db.Get([]byte(LastMigratedNonAncientBlockKey))
-	if err != nil {
-		return 0
-	}
-	number := binary.BigEndian.Uint64(data)
-	return number
-}
-
-// writeLastMigratedNonAncientBlock stores the last migration number.
-func writeLastMigratedNonAncientBlock(db ethdb.KeyValueWriter, number uint64) error {
-	enc := make([]byte, 8)
-	binary.BigEndian.PutUint64(enc, number)
-	return db.Put([]byte(LastMigratedNonAncientBlockKey), enc)
-}
-
-// deleteLastMigratedNonAncientBlock removes the last migration number.
-func deleteLastMigratedNonAncientBlock(db ethdb.KeyValueWriter) error {
-	return db.Delete([]byte(LastMigratedNonAncientBlockKey))
 }
 
 // openDB opens the chaindata database at the given path. Note this path is below the datadir
