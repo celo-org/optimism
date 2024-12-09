fd44a81b25d44626188c86200985f76cb6a25449
kourin <yoshiki.takabayashi@gmail.com>
2024-12-09 20:30:27 +0900

    Resolve fuzzing test failures for AddressSortedLinkedListWithMedian (#277)
    * Ignore AddressSortedLinkedList and AddressSortedLinkedListWithMedian as targets in fuzzing test of SafeCall_Test
    
    * Format test/libraries/SafeCall.t.sol

--- packages/contracts-bedrock/test/libraries/SafeCall.t.sol
+++ packages/contracts-bedrock/test/libraries/SafeCall.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.15;
 // Testing utilities
 import { Test } from "forge-std/Test.sol";
 import { StdCheatsSafe } from "forge-std/StdCheats.sol";
+import { AddressSortedLinkedList } from "src/celo/common/linkedlists/AddressSortedLinkedList.sol";
+import { AddressSortedLinkedListWithMedian } from "src/celo/common/linkedlists/AddressSortedLinkedListWithMedian.sol";
 
 // Target contract
 import { SafeCall } from "src/libraries/SafeCall.sol";
@@ -14,7 +16,10 @@ contract SafeCall_Test is Test {
         vm.assume(_addr.balance == 0);
         vm.assume(_addr != address(this));
         vm.assume(uint256(uint160(_addr)) > uint256(256)); // TODO temp fix until new forge-std release with modern
-            // precompiles: https://github.com/foundry-rs/forge-std/pull/594
+        // ignore address of library contract whose functions have 'public' or 'external' visibilities
+        vm.assume(_addr != address(AddressSortedLinkedList));
+        vm.assume(_addr != address(AddressSortedLinkedListWithMedian));
+        // precompiles: https://github.com/foundry-rs/forge-std/pull/594
         assumeAddressIsNot(_addr, StdCheatsSafe.AddressType.ForgeAddress, StdCheatsSafe.AddressType.Precompile);
     }
 
