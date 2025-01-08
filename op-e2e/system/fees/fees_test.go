3cef081c2d5a2bc29f384b6740db0c35eea94f9e
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2025-01-08 12:49:28 +0100

    Adapt address-getters to op-geth library changes

--- op-e2e/system/fees/fees_test.go
+++ op-e2e/system/fees/fees_test.go
@@ -137,7 +137,8 @@ func testFees(t *testing.T, cfg e2esys.SystemConfig) {
 
 	baseFeeRecipient := predeploys.BaseFeeVaultAddr
 	if sys.RollupConfig.IsCel2(sys.L2GenesisCfg.Timestamp) {
-		baseFeeRecipient = addresses.FeeHandlerAddress
+		// We use the mainnet addresses in tests
+		baseFeeRecipient = addresses.GetAddresses(big.NewInt(params.CeloMainnetChainID)).FeeHandler
 	}
 	// BaseFee Recipient
 	baseFeeRecipientStartBalance, err := l2Seq.BalanceAt(context.Background(), baseFeeRecipient, big.NewInt(rpc.EarliestBlockNumber.Int64()))
