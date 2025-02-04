2bc3c82c8f9cbdcf72c35f60787fa60ed9c1b157
Karl Bartel <karl.bartel@clabs.co>
2025-02-04 15:22:22 +0100

    op-e2e: Skip regolith tests
    These clash with the check in
    https://github.com/celo-org/op-geth/commit/2a740e5a52bbe29e322bbdb750993d3f52199e1a
    and are safe to ignore, since the regolith fork is always enabled on
    Cel2 chains, so that we'll never migrate to it.

--- op-e2e/opgeth/op_geth_test.go
+++ op-e2e/opgeth/op_geth_test.go
@@ -237,6 +237,7 @@ func TestGethOnlyPendingBlockIsLatest(t *testing.T) {
 }
 
 func TestPreregolith(t *testing.T) {
+	t.Skip("Not applicable to Celo chains")
 	futureTimestamp := hexutil.Uint64(4)
 	tests := []struct {
 		name         string
@@ -418,6 +419,7 @@ func TestPreregolith(t *testing.T) {
 }
 
 func TestRegolith(t *testing.T) {
+	t.Skip("Not applicable to Celo chains")
 	tests := []struct {
 		name             string
 		regolithTime     hexutil.Uint64
