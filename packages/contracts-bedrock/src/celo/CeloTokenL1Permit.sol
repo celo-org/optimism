71b14199c992621359fd119e454a8f7bac53d7ad
Martin <martin@clabs.co>
2025-02-27 16:41:52 +0100

    Move constants inside contracts (#336)

--- packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
+++ packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 import { ERC20Permit } from "@openzeppelin/contracts-v5/token/ERC20/extensions/ERC20Permit.sol";
 import { ERC20 } from "@openzeppelin/contracts-v5/token/ERC20/ERC20.sol";
 
-string constant NAME = "Celo native asset";
-string constant SYMBOL = "CELO";
-uint256 constant TOTAL_MARKET_CAP = 1000000000e18; // 1 billion CELO
-
 contract CeloTokenL1Permit is ERC20Permit {
+    string constant NAME = "Celo native asset";
+    string constant SYMBOL = "CELO";
+    uint256 constant TOTAL_MARKET_CAP = 1000000000e18; // 1 billion CELO
+
     constructor(address portalProxyAddress) ERC20(NAME, SYMBOL) ERC20Permit(NAME) {
         _mint(portalProxyAddress, TOTAL_MARKET_CAP);
     }
