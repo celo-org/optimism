17cbfd820fec81742a3f2a97ce32d0f12fc05947
Marcin Chrzanowski <martin@clabs.co>
2025-03-06 20:14:43 +0100

    Add new config to go and template

--- op-chain-ops/genesis/config.go
+++ op-chain-ops/genesis/config.go
@@ -910,6 +910,9 @@ type DeployConfig struct {
 
 	// DeployCeloContracts indicates whether to deploy Celo contracts.
 	DeployCeloContracts bool `json:"deployCeloContracts"`
+
+	// Used to validate the ProxyAdmin owner address.
+	ProxyAdminOwnerIsMultisig bool `json:"proxyAdminOwnerIsMultisig"`
 }
 
 // Copy will deeply copy the DeployConfig. This does a JSON roundtrip to copy
