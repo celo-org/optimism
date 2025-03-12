4ef29a72ee2b9b076ea906a0cefe9636175e5ef6
Martín Volpe <martin.volpe@gmail.com>
2025-03-12 09:03:40 -0300

    Update packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol

--- packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
+++ packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import { ERC20Permit } from "@openzeppelin/contracts-v5/token/ERC20/extensions/ERC20Permit.sol";
 import { ERC20 } from "@openzeppelin/contracts-v5/token/ERC20/ERC20.sol";
 
-
-
 contract CeloTokenL1Permit is ERC20Permit {
     string constant NAME = "Celo";
     string constant SYMBOL = "CELO";
