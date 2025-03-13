a0b9c4a46783ea726f58e62a5103178b079e0270
Martín Volpe <martin.volpe@gmail.com>
2025-03-13 13:19:54 -0300

    Fix Celo token

--- packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
+++ packages/contracts-bedrock/src/celo/CeloTokenL1Permit.sol
@@ -5,7 +5,7 @@ import { ERC20Permit } from "@openzeppelin/contracts-v5/token/ERC20/extensions/E
 import { ERC20 } from "@openzeppelin/contracts-v5/token/ERC20/ERC20.sol";
 
 contract CeloTokenL1Permit is ERC20Permit {
-    string constant NAME = "Celo native asset";
+    string constant NAME = "Celo";
     string constant SYMBOL = "CELO";
     uint256 constant TOTAL_MARKET_CAP = 1000000000e18; // 1 billion CELO
 
