cec853b1c950aa3fcca949fc463fffb658206c11
Martín Volpe <martin.volpe@gmail.com>
2025-03-13 13:15:50 -0300

    Update CeloTokenL1.sol

--- packages/contracts-bedrock/src/celo/CeloTokenL1.sol
+++ packages/contracts-bedrock/src/celo/CeloTokenL1.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract CeloTokenL1 is ERC20Upgradeable {
-    string constant NAME = "Celo native asset";
+    string constant NAME = "Celo";
     string constant SYMBOL = "CELO";
     uint256 constant TOTAL_MARKET_CAP = 1000000000e18; // 1 billion CELO
 
