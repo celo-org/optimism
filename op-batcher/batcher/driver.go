00154d6734e6071f5677002b40157511d47a67c4
kourin <yoshiki.takabayashi@gmail.com>
2025-04-03 19:45:04 +0900

    Add Prometheus metrics for AltDA failover in Batcher (#361)
    * Add Prometheus metrics for AltDA failover in Batcher
    
    * Fix calls of RecordBatchDaType
    
    * Revert deleted comment
    
    * Add metrics for total stored batch size
    
    * Fix condition for RecordBatchDaType and RecordBatchDataSizeBytes
    
    * Add missing namespace to prometheus definition
    
    * Fix the amount of batch sizes recorded for DA
    
    * Unify recordings of size of batch to be stored
    
    * Improve Prometheus help text

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -47,6 +47,8 @@ type txRef struct {
 	id       txID
 	isCancel bool
 	isBlob   bool
+	daType   DaType
+	size     int
 }
 
 func (r txRef) String() string {
@@ -910,7 +912,7 @@ func (l *BatchSubmitter) sendTx(txdata txData, isCancel bool, candidate *txmgr.T
 		candidate.GasLimit = floorDataGas
 	}
 
-	queue.Send(txRef{id: txdata.ID(), isCancel: isCancel, isBlob: txdata.daType == DaTypeBlob}, *candidate, receiptsCh)
+	queue.Send(txRef{id: txdata.ID(), isCancel: isCancel, isBlob: txdata.daType == DaTypeBlob, daType: txdata.daType, size: txdata.Len()}, *candidate, receiptsCh)
 }
 
 // Copypaste from upstream geth
@@ -963,6 +965,10 @@ func (l *BatchSubmitter) handleReceipt(r txmgr.TxReceipt[txRef]) {
 		l.recordFailedTx(r.ID.id, r.Err)
 	} else if r.Receipt != nil {
 		l.recordConfirmedTx(r.ID.id, r.Receipt)
+		if !r.ID.isCancel {
+			l.Metr.RecordBatchDaType(r.ID.daType.Name())
+			l.Metr.RecordBatchDataSizeBytes(r.ID.daType.Name(), r.ID.size)
+		}
 	}
 	// Both r.Err and r.Receipt can be nil, in which case we do nothing.
 }
