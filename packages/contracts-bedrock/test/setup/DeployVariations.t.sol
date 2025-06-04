cdba802eb5e374fa3aa387deaa3243f219f821a1
pahor167 <hornak.pavel@gmail.com>
2025-06-04 11:35:46 +0200

    removal of  CustomGasTokenNotSupported() error

--- packages/contracts-bedrock/test/setup/DeployVariations.t.sol
+++ packages/contracts-bedrock/test/setup/DeployVariations.t.sol
@@ -5,8 +5,6 @@ pragma solidity 0.8.15;
 import { CommonTest } from "test/setup/CommonTest.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-error CustomGasTokenNotSupported();
-
 contract DeployVariations_Test is CommonTest {
     function setUp() public override {
         // Prevent calling the base CommonTest.setUp() function, as we will run it within the test functions
@@ -16,7 +14,6 @@ contract DeployVariations_Test is CommonTest {
     // Enable features which should be possible to enable or disable regardless of other options.
     function enableAddOns(bool _enableCGT, bool _enableAltDa) public {
         if (_enableCGT) {
-            if (true) revert CustomGasTokenNotSupported();
 
             ERC20 token = new ERC20("Silly", "SIL");
             super.enableCustomGasToken(address(token));
