c102ced69abe22ec88a50a21377552a9a9cf0933
Artemii Gerasimovich <artemii@espressosys.com>
2026-05-20 21:51:33 +0200

    Lower pragma

--- packages/contracts-bedrock/scripts/deploy/DeployEspresso.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployEspresso.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.0;
 
 import { BaseDeployIO } from "scripts/deploy/BaseDeployIO.sol";
 import { Script } from "forge-std/Script.sol";
